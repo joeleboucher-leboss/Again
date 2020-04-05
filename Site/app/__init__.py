57d2e541465625debda80879fcf6f34a3d375876
Thomas DRAKE <thomas.drake@live.fr>
2020-04-06 01:40:25 +0200

    Normalement tout devrait fonctionner maintenant

--- Site/app/__init__.py
+++ Site/app/__init__.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-from app import routes
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.htm')
