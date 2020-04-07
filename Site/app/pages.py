9f3e17c10ad4ad2da6ca8769d58a3796e1441629
Thomas DRAKE <thomas.drake@live.fr>
2020-04-07 10:24:55 +0200

    Création de la base de données locale again (pour le développement)

--- Site/app/pages.py
+++ Site/app/pages.py
@@ -1,16 +1,37 @@
 from flask import render_template, redirect
 from app import app
 from . import config
-
+import sqlite3
+import os
 
 @app.route('/')
 @app.route('/index')
 def index():
-    if config.maintenance == True: #On remplacera cette ligne par une recherche dans la base de données du Site
+    #On va d'abord chercher les données relatives au site
+    ##region requête SQL
+    conn = sqlite3.connect("again.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM donnees_site")
+    donnees_site = cur.fetchone()
+    conn.close()
+    ##endregion
+
+    if donnees_site['maintenance'] == 1: #On remplacera cette ligne par une recherche dans la base de données du Site
         return redirect('/maintenance')
     return render_template('index.htm')
+
 @app.route('/maintenance')
 def maintenance():
-    if config.maintenance == False: #On remplacera cette ligne par une recherche dans la base de données du Site
+    ##region requête SQL
+    conn = sqlite3.connect("again.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM donnees_site")
+    donnees_site = cur.fetchone()
+    conn.close()
+    ##endregion
+
+    if donnees_site['maintenance'] != 1: #On remplacera cette ligne par une recherche dans la base de données du Site
         return redirect('/')
     return render_template('maintenance.htm')
