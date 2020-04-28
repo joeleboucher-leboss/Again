159d356277848035325fc83551e03f27a8e7a52e
Thomas DRAKE <thomas.drake@live.fr>
2020-04-28 15:03:20 +0200

    Pages vierges pour Baba

---
+++ Site/app/purchase.py
@@ -0,0 +1,20 @@
+from flask import render_template, redirect, url_for
+from flask import request
+from werkzeug.urls import url_parse
+#from jinja2 import Template #pour déboguer
+from app import app, db, login
+from flask_babel import _, refresh
+from flask_login import current_user, login_user, logout_user
+from app.models import User, Category, Prize
+from app.config import Config
+from app.forms import LoginForm, RegistrationForm, Admin_NewCategoryForm, Admin_NewStandartProduct
+import os
+import app.admin_modules as modules
+
+@app.route("/view-prize")
+def view_prize():
+    return render_template("purchase/display_product.htm", title=_('Article'))
+
+@app.route("/purchase-confirm")
+def confirm_purchase():
+    return render_template("purchase/purchase_validated.htm", title=_('Confirmation'))
