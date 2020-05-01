3dbc62940174725b12c7e26bed1818abbad55ad9
Thomas DRAKE <thomas.drake@live.fr>
2020-05-01 08:20:10 +0200

    Apparence dashboard travaillée, code commenté, barre de navigation fixée

--- Site/app/__init__.py
+++ Site/app/__init__.py
@@ -5,15 +5,15 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_babel import Babel
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-babel = Babel(app)
+app = Flask(__name__) # Création de l'application flask
+app.config.from_object(Config) # Initialisation des paramètres de configuration à partir du fichier config.py
+db = SQLAlchemy(app) # Création d'un gestionnaire de base de données
+migrate = Migrate(app, db) # Création d'un gestionnaire de migrations
+login = LoginManager(app) # Création d'un gestionnaire d'authentification
+babel = Babel(app) # Création d'un gestionnaire de langues
 
 @babel.localeselector
-def get_locale():
+def get_locale(): # Permet d'obtenir la langue la plus appropriée par rapport au navigateur de l'utilisateur. Fonction directement utilisée par Babel
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 from app import pages, models
--- Site/app/forms.py
+++ Site/app/forms.py
@@ -4,12 +4,14 @@ from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from flask_babel import lazy_gettext as _
 from app.models import User
 
+# Formulaire de connexion
 class LoginForm(FlaskForm):
     username = StringField(_('pseudo'), validators=[DataRequired()])
     password = PasswordField(_('mot de passe'), validators=[DataRequired()])
     remember_me = BooleanField(_('se souvenir de moi'))
     submit = SubmitField(_('connexion'))
 
+# Formulaire d'inscription
 class RegistrationForm(FlaskForm):
     username = StringField(_("pseudo"), validators=[DataRequired()])
     email = StringField(_('e-mail'), validators=[DataRequired(), Email()])
@@ -18,21 +20,24 @@ class RegistrationForm(FlaskForm):
         _('confirmer le mot de passe'), validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField(_("S'enregistrer"))
 
+    # Méthode de vérification du nom d'utilisateur
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
+        user = User.query.filter_by(username=username.data).first() # On recherche un utilisateur du même nom
         if user is not None:
-            raise ValidationError(_('Veuillez choisir un autre pseudo'))
+            raise ValidationError(_('Veuillez choisir un autre pseudo')) # Sil y en existe un, alors on renvoie une erreur à la page
 
+    # Méthode de vérification de l'adresse e-mail
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
+        user = User.query.filter_by(email=email.data).first() # On recherche une adresse e-mail identique
         if user is not None:
-            raise ValidationError(_('Veuillez utiliser une autre adresse e-mail'))
-
+            raise ValidationError(_('Veuillez utiliser une autre adresse e-mail')) # S'il en existe une, on renvoie un erreur à la page
+# Formulaire de création de nouvelle catégorie (administrateur)
 class Admin_NewCategoryForm(FlaskForm):
     name = StringField(_('nom'), validators=[DataRequired()])
     parent_id = HiddenField()
     newCategorySubmit = SubmitField(_('ajouter'))
 
+# Formulaire de création de nouveau produit standart (administrateur)
 class Admin_NewStandartProduct(FlaskForm):
     category = HiddenField()
     name = StringField(_('nom'), validators=[DataRequired()])
